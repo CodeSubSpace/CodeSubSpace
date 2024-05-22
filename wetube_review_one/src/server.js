f4a406e131b28e4a9bfc5bea93bc7e0d17b56dc9
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-22 16:30:10 +0900

    #3.3 Express는 req, res라는 객체를 제공한다.

--- wetube_review_one/src/server.js
+++ wetube_review_one/src/server.js
@@ -4,10 +4,11 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-    console.log(req);
+    console.log(req.route.path);
     res.send("This is Root Page!")
 }
 app.get("/", handleHome);
+app.get("/page2", handleHome);
 
 const handleListener = () => {
     console.log (`✅ The server listening on port 4000 http://localhost:${PORT} 🚀`)
