818acd76252aebff2e4eb12b3b7baf26f16f849d
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-22 15:01:38 +0900

    #3.1 client의 GET request에 대하여 server가 response를 준다

--- wetube_review_one/src/server.js
+++ wetube_review_one/src/server.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 const app = express();
 
 const handleListener = () => {
-    console.log (`The server listening on port 4000 http://localhost:${PORT} 🚀`)
+    console.log (`✅ The server listening on port 4000 http://localhost:${PORT} 🚀`)
 }
 
 app.listen(PORT, handleListener);
