8ac7b6eb341ce5b897e5b8471a62923aa184b669
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-22 15:43:08 +0900

    #3.2 Request part Two

--- wetube_review_one/src/server.js
+++ wetube_review_one/src/server.js
@@ -3,6 +3,12 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+    console.log(req);
+    res.send("This is Root Page!")
+}
+app.get("/", handleHome);
+
 const handleListener = () => {
     console.log (`✅ The server listening on port 4000 http://localhost:${PORT} 🚀`)
 }
