4886570f90fcdccd922317ff88a5111930477a0e
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-22 14:39:45 +0900

    #3.0 express package를 이용하여 server 열기(req에 대한 listening 상태)

---
+++ wetube_review_one/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleListener = () => {
+    console.log (`The server listening on port 4000 http://localhost:${PORT} 🚀`)
+}
+
+app.listen(PORT, handleListener);
