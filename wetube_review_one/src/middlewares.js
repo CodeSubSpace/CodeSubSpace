6c3ac5f32b362e0d30c6090b9731a222d6090e61
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-23 16:10:50 +0900

    #3.11 Morgan은 req에 대한 여러 정보를 console에 띄워주는 미들웨어를 제공하는 node.JS 패키지이다.

---
+++ wetube_review_one/src/middlewares.js
@@ -0,0 +1,3 @@
+import morgan from "morgan";
+
+export const logger = morgan("dev"); // next()가 logger 함수에 내장되어 있다.
--- wetube_review_one/src/server.js
+++ wetube_review_one/src/server.js
@@ -1,20 +1,15 @@
 import express from "express";
+import { logger } from "./middlewares";
 
 const PORT = 4000;
 const app = express();
 
-const testMiddleware = (req, res, next) => {
-    // something is change... (may be req changes...!)
-    console.log("hello! i'm middleware!")
-    next()
-}
-
 const handleHome = (req, res) => {
-    console.log(req.route.path);
     res.send("This is Root Page!")
 }
-app.get("/", testMiddleware, handleHome);
-app.get("/page2", handleHome);
+
+app.use(logger);
+app.get("/", handleHome)
 
 const handleListener = () => {
     console.log (`✅ The server listening on port 4000 http://localhost:${PORT} 🚀`)
