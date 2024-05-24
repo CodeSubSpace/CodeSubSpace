6d5e1410c3538992902bd5c0abb147a3adfe8c22
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-24 15:03:29 +0900

    #5 backend 수준에서 PUG node.JS 패키지로 프론트엔드 디자인 및 로드하기

--- wetube_review_one/src/routers/videoRouter.js
+++ wetube_review_one/src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { edit, see, seeParams } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/", see);
-videoRouter.get("/:id(\\d+)", seeParams);
-videoRouter.get("/edit", edit)
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
-export default videoRouter
+export default videoRouter;
--- wetube_review_one/src/server.js
+++ wetube_review_one/src/server.js
@@ -2,18 +2,24 @@ import express from "express";
 import { logger } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 
 // Server setting
 const PORT = 4000;
 const app = express();
+    // PUG setting
+    app.set("view engine", "pug");
+    app.set("views", process.cwd() + "/src/views");
 
 // Middleware
 app.use(logger);
 
+
 // Router
 app.use("/", globalRouter);
-app.use("/video", videoRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 
 // ✅ Open Server 🚀
